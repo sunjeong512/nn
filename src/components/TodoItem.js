b13037c2b34ae5506ef0264aa37440e9060247cd
MyeongwonKim <mw7539@naver.com>
2023-04-25 10:58:24 +0900

    edit 버튼 추가했습니다

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,12 +4,15 @@
   삭제 버튼을 통해 해당 할 일을 삭제할 수 있습니다.
   이 컴포넌트는 `TodoList.js`에서 사용되어 할 일 목록을 구성합니다.
 */
-import React from "react";
+import React, {useState} from "react";
 import styles from "@/styles/TodoList.module.css";
 
 // TodoItem 컴포넌트를 정의합니다.
 const TodoItem = ({ todo, onToggle, onDelete }) => {
   // 각 할 일 항목을 렌더링합니다.
+  let [isEdited, setIsEdited] = useState(false);
+
+
   return (
     <li className={styles.todoItem}>
       {/* 체크박스를 렌더링하고, 체크박스의 상태를 할 일의 완료 상태와 동기화합니다.
@@ -17,14 +20,12 @@ const TodoItem = ({ todo, onToggle, onDelete }) => {
       <input type="checkbox" checked={todo.completed} onChange={onToggle} />
 
       {/* 할 일의 텍스트를 렌더링하고, 완료 상태에 따라 텍스트에 취소선을 적용합니다. */}
-      <span
-        className={styles.todoText}
-        style={{ textDecoration: todo.completed ? "line-through" : "none" }}
-      >
+      <span className={styles.todoText} style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
         {todo.text}
       </span>
 
       {/* 삭제 버튼을 렌더링하고, 클릭 시 onDelete 함수를 호출하여 해당 할 일을 삭제합니다. */}
+      <button onClick={onDelete}>Edit</button>
       <button onClick={onDelete}>Delete</button>
     </li>
   );
