4900e2a7ce90808befd9c59542e7aefdf78cabf6
MyeongwonKim <mw7539@naver.com>
2023-04-25 11:18:44 +0900

    수정 버튼을 누르면 기존 텍스트가 수정란으로 변하고, 수정 버튼은 완료 버튼이 됩니다.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -10,22 +10,61 @@ import styles from "@/styles/TodoList.module.css";
 // TodoItem 컴포넌트를 정의합니다.
 const TodoItem = ({ todo, onToggle, onDelete }) => {
   // 각 할 일 항목을 렌더링합니다.
-  let [isEdited, setIsEdited] = useState(false);
+  let [isEditing, setIsEditing] = useState(false);
+  let [editText, setEditText] = useState(todo.text)
+
+  const editTodo = () => {
+    setIsEditing(true)
+  }
+
+  const editComplete = () => {
+    setIsEditing(false)
+  }
 
 
   return (
     <li className={styles.todoItem}>
       {/* 체크박스를 렌더링하고, 체크박스의 상태를 할 일의 완료 상태와 동기화합니다.
-          체크박스의 상태가 변경되면 onToggle 함수를 호출하여 완료 상태를 업데이트합니다. */}
-      <input type="checkbox" checked={todo.completed} onChange={onToggle} />
+          체크박스의 상태가 변경되면 onToggle 함수를 호출하여 완료 상태를 업데이트합니다. 
+          수정 버튼을 누르면 기존의 할일 텍스트가 수정창으로 변합니다.    
+      */}
+
+      {
+        isEditing
+          ? null
+          : <input
+            type="checkbox" checked={todo.completed} onChange={onToggle} />
+      }
+
+      
 
       {/* 할 일의 텍스트를 렌더링하고, 완료 상태에 따라 텍스트에 취소선을 적용합니다. */}
-      <span className={styles.todoText} style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
-        {todo.text}
-      </span>
+      {
+        isEditing
+          ? (<input
+              type="text"
+              value={editText}
+              onChange={(e) => {setEditText(e.target.value)}}
+          />)
+          
+          : (<span
+              className={styles.todoText}
+              style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
+              {editText}
+            </span>
+            )
+      }
+      
+
+      
+
+      {/* 수정 버튼을 렌더링 하고, 클릭 시 수정 창을 활성화하고, 수정 버튼은 완료 버튼이 됩니다. */}
+      {
+        isEditing
+          ? <button onClick={editComplete}>Complete</button>
+          : <button onClick={editTodo}>Edit</button>}
 
       {/* 삭제 버튼을 렌더링하고, 클릭 시 onDelete 함수를 호출하여 해당 할 일을 삭제합니다. */}
-      <button onClick={onDelete}>Edit</button>
       <button onClick={onDelete}>Delete</button>
     </li>
   );
