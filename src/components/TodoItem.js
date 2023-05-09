ccf70a558b412ac899f6ee70348644a2cd2e0436
sunjeong512 <select@snu.ac.kr>
2023-05-09 19:19:11 +0900

    ss

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,134 +4,35 @@
   삭제 버튼을 통해 해당 할 일을 삭제할 수 있습니다.
   이 컴포넌트는 `TodoList.js`에서 사용되어 할 일 목록을 구성합니다.
 */
-import React, { useState } from "react";
+import React from "react";
 import styles from "@/styles/TodoList.module.css";
 
 // TodoItem 컴포넌트를 정의합니다.
 const TodoItem = ({ todo, onToggle, onDelete }) => {
   // 각 할 일 항목을 렌더링합니다.
-  let [isEditing, setIsEditing] = useState(false);
-  let [editText, setEditText] = useState(todo.text);
-  let [blankAlert, setBlankAlert] = useState(false);
-
-  const emojiList = ["😃", "😐", "😔"];
-
-  const [selectedEmoji, setSelectedEmoji] = useState(null); // 선택된 이모지를 상태로 관리
-
-  const handleEmojiToggle = (emoji) => {
-    // 이모지를 선택하면 선택된 이모지를 상태로 업데이트
-    setSelectedEmoji(emoji);
-  };
-
-  const editTodo = () => {
-    setIsEditing(true);
-  };
-
-  const editComplete = () => {
-    if (editText.trim() == "") {
-      setBlankAlert(true);
-      return;
-    }
-    setIsEditing(false);
-  };
-
   return (
-    <ul>
-      <li className={styles.todoItem}>
-        <li className="mt-9 ml-3"></li>
-        {/* 체크박스를 렌더링하고, 체크박스의 상태를 할 일의 완료 상태와 동기화합니다.
-            체크박스의 상태가 변경되면 onToggle 함수를 호출하여 완료 상태를 업데이트합니다. 
-            수정 버튼을 누르면 기존의 할일 텍스트가 수정창으로 변합니다.    
-        */}
+    <li className={styles.todoItem}>
+      {/* 체크박스를 렌더링하고, 체크박스의 상태를 할 일의 완료 상태와 동기화합니다.
+          체크박스의 상태가 변경되면 onToggle 함수를 호출하여 완료 상태를 업데이트합니다. */}
+      <input type="checkbox" checked={todo.completed} onChange={onToggle} />
 
-        {isEditing ? null : (
-          <input type="checkbox" checked={todo.completed} onChange={onToggle} />
-        )}
+      {/* 할 일의 텍스트를 렌더링하고, 완료 상태에 따라 텍스트에 취소선을 적용합니다. */}
+      <span
+        className={styles.todoText}
+        style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+      >
+        {todo.text}
+      </span>
 
-        {/* 할 일의 텍스트를 렌더링하고, 완료 상태에 따라 텍스트에 취소선을 적용합니다. */}
-        {isEditing ? (
-          <input
-            className={styles.editInput}
-            type="text"
-            value={editText}
-            onChange={(e) => {
-              setEditText(e.target.value);
-            }}
-          />
-        ) : (
-          <span
-            className="mt-0 mb-0.5 ml-4 mr-auto"
-            style={{ textDecoration: todo.completed ? "line-through" : "none" }}
-          >
-            {editText}
-          </span>
-        )}
+      <span>
+        {todo.datetime}
+      </span>
 
-        {/* 수정 버튼을 렌더링 하고, 클릭 시 수정 창을 활성화하고, 수정 버튼은 완료 버튼이 됩니다. */}
-      {isEditing ? (
-        <button className="bg-white text-[#8b5cf6] font-normal mb-0.5 py-0.5 px-3 mr-2 rounded-2xl border border-white hover:border-[#8b5cf6]" onClick={editComplete}>Complete</button>
-      ) : (
-        <button className="bg-white text-[#8b5cf6] font-normal mb-0.5 py-0.5 px-3 mr-2 rounded-2xl border border-white hover:border-[#8b5cf6]" onClick={editTodo}>Edit</button>
-      )}
 
-        {/* 삭제 버튼을 렌더링하고, 클릭 시 onDelete 함수를 호출하여 해당 할 일을 삭제합니다. */}
-    
-        <button className="bg-[#ede9fe] text-[#8b5cf6] font-normal mb-0.5 py-0.5 px-3 rounded-2xl border border-[#ede9fe] hover:border-[#8b5cf6]" onClick={onDelete}>Delete</button>
+      {/* 삭제 버튼을 렌더링하고, 클릭 시 onDelete 함수를 호출하여 해당 할 일을 삭제합니다. */}
+      <button className="bg-[#ede9fe] text-[#8b5cf6] font-normal mb-0.5 py-0.1 px-2 ml-3 rounded-2xl border border-[#ede9fe] hover:border-[#8b5cf6]" onClick={onDelete}>Delete</button>
 
-        {blankAlert ? (
-          <div className="fixed inset-0 w-full rounded-3xl bg-white border-10 flex flex-col justify-center items-center">
-            <h2 className="mb-8 text-xl font-bold">빈칸을 채워주세요!</h2>
-            <div className="flex">
-              <button
-                className="mr-5 w-40 justify-self-end p-1 mb-4 bg-[#8b5cf6] text-white border border-[#8b5cf6] rounded hover:bg-white hover:text-[#8b5cf6]"
-                onClick={() => {
-                  setBlankAlert(false);
-                }}
-              >
-                확인
-              </button>
-            </div>
-          </div>
-        ) : null}
-      </li>
-      <li className={styles.todoItem}>
-        <li className="bg-[#f5f3ff] flex items-center justify-between px-4 pt-1 pb-0 pr-5 rounded-2xl">
-          <p className="whitespace-normal text-[#8b5cf6] text-sm mb-2 font-normal mr-52 mt-1">
-            위 할 일을 만족스럽게 수행하셨나요? </p>
-        {/* 선택된 이모지에 따라 배경 색을 보라색으로 변경 */}
-        <div
-          className="ml-0.5 mb-1.5"
-          style={{
-            backgroundColor:
-              selectedEmoji === emojiList[0] ? "#8b5cf6" : "transparent",
-          }}
-          onClick={() => handleEmojiToggle(emojiList[0])}
-        >
-          {emojiList[0]}
-        </div>
-        <div
-          className="ml-2 mb-1.5"
-          style={{
-            backgroundColor:
-              selectedEmoji === emojiList[1] ? "#8b5cf6" : "transparent",
-          }}
-          onClick={() => handleEmojiToggle(emojiList[1])}
-        >
-          {emojiList[1]}
-        </div>
-        <div
-          className="ml-2 mb-1.5"
-          style={{
-            backgroundColor:
-              selectedEmoji === emojiList[2] ? "#8b5cf6" : "transparent",
-          }}
-          onClick={() => handleEmojiToggle(emojiList[2])}
-        >
-          {emojiList[2]}
-        </div>
-        </li>
-      </li>
-    </ul>
+    </li>
   );
 };
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,20 +3,52 @@
   상태 관리를 위해 `useState` 훅을 사용하여 할 일 목록과 입력값을 관리합니다.
   할 일 목록의 추가, 삭제, 완료 상태 변경 등의 기능을 구현하였습니다.
 */
-import React, { useState } from "react";
+
+import React, { useState, useEffect } from "react";
 import TodoItem from "@/components/TodoItem";
 import styles from "@/styles/TodoList.module.css";
+
+import { db } from "@/firebase";
+import {
+  collection,
+  query,
+  doc,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  orderBy,
+} from "firebase/firestore";
+
+const todoCollection = collection(db,"todos");
+
 
 // TodoList 컴포넌트를 정의합니다.
 const TodoList = () => {
   // 상태를 관리하는 useState 훅을 사용하여 할 일 목록과 입력값을 초기화합니다.
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
-  const [deleteModal, setDeleteModal] = useState(false);
-  const [deleteID, setDeleteID] = useState(0);
+
+  const getTodos = async () => {
+    const q = query(todoCollection, orderBy("datetime", "desc"));
+
+    const results = await getDocs(q);
+    const newTodos = [];
+
+    results.docs.forEach((doc) => {
+      newTodos.push({ id: doc.id, ...doc.data() });
+    });
+
+    setTodos(newTodos);
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
 
   // addTodo 함수는 입력값을 이용하여 새로운 할 일을 목록에 추가하는 함수입니다.
-  const addTodo = () => {
+  const addTodo = async () => {
     // 입력값이 비어있는 경우 함수를 종료합니다.
     if (input.trim() === "") return;
     // 기존 할 일 목록에 새로운 할 일을 추가하고, 입력값을 초기화합니다.
@@ -26,26 +58,44 @@ const TodoList = () => {
     //   completed: 완료 여부,
     // }
     // ...todos => {id: 1, text: "할일1", completed: false}, {id: 2, text: "할일2", completed: false}}, ..
-    setTodos([...todos, { id: Date.now(), text: input, completed: false }]);
+    const date = new Date().toISOString().split("T")[0];
+    const time = new Date().toTimeString().split(" ")[0];
+    const daytime = date + " " + time
+
+
+    const docRef = await addDoc(todoCollection, {
+      text: input,
+      completed: false,
+      datetime: daytime
+    });
+
+
+    setTodos([{ id: docRef.id, text: input, completed: false, datetime: daytime }, ...todos ]);
     setInput("");
+    
   };
 
   // toggleTodo 함수는 체크박스를 눌러 할 일의 완료 상태를 변경하는 함수입니다.
   const toggleTodo = (id) => {
     // 할 일 목록에서 해당 id를 가진 할 일의 완료 상태를 반전시킵니다.
-    setTodos(
-      // todos.map((todo) =>
-      //   todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      // )
-      // ...todo => id: 1, text: "할일1", completed: false
-      todos.map((todo) => {
-        return todo.id === id ? { ...todo, completed: !todo.completed } : todo;
-      })
-    );
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        const todoDoc = doc(todoCollection, id);
+        updateDoc(todoDoc, { completed:!todo.completed });
+        return { ...todo, completed:!todo.completed }; 
+      } else { 
+        return todo;
+      }
+    });
+
+    setTodos(newTodos);
   };
 
   // deleteTodo 함수는 할 일을 목록에서 삭제하는 함수입니다.
-  const deleteTodo = async (id) => {
+  const deleteTodo = (id) => {
+    const todoDoc = doc(todoCollection, id);
+    deleteDoc(todoDoc);
+
     // 해당 id를 가진 할 일을 제외한 나머지 목록을 새로운 상태로 저장합니다.
     // setTodos(todos.filter((todo) => todo.id !== id));
     setTodos(
@@ -55,16 +105,10 @@ const TodoList = () => {
     );
   };
 
-  const deleteTry = (id) => {
-    setDeleteModal(true);
-    setDeleteID(id);
-  }
-
-
   // 컴포넌트를 렌더링합니다.
   return (
     <div className={styles.container}>
-      <h1 className="animate-bounce text-xl mb-4 font-bold underline underline-offset-4 decoration-wavy decoration-[#8b5cf6]">
+      <h1 className="animate-bounce text-xl mb-4 font-bold underline underline-offset-4 decoration-wavy">
         Todo List
       </h1>
       {/* 할 일을 입력받는 텍스트 필드입니다. */}
@@ -77,7 +121,7 @@ const TodoList = () => {
         //   padding: 5px;
         //   margin-bottom: 10px;
         // }
-        className="w-full p-1 mb-4 border border-[#8b5cf6] rounded-2xl"
+        className="shadow-lg shadow-[#8b5cf6]/20 w-full p-1 mb-4 border border-[#8b5cf6] rounded-2xl"
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
@@ -99,50 +143,25 @@ const TodoList = () => {
           //   background-color: #fff;
           //   color: #0070f3;
           // }
-          
-          className="shadow-lg shadow-[#8b5cf6]/40 w-40 justify-self-end p-1 mb-10 bg-[#8b5cf6] text-white border border-[#8b5cf6] rounded-2xl hover:bg-white hover:text-[#8b5cf6]"
+          className="w-40 justify-self-end p-1 mb-4 bg-[#8b5cf6] text-white border border-[#8b5cf6] rounded-2xl hover:bg-[#ddd6fe] hover:border-[#ddd6fe] hover:text-[#8b5cf6]"
           onClick={addTodo}
         >
           Add Todo
         </button>
       </div>
-    
-
       {/* 할 일 목록을 렌더링합니다. */}
       <ul>
         {todos.map((todo) => (
-              <TodoItem
-              key={todo.id}
-              todo={todo}
-              onToggle={() => toggleTodo(todo.id)}
-              onDelete={() => deleteTry(todo.id)}
-              // onDelete={() => confirm("정말 삭제하시겠습니까?") && deleteTodo(todo.id)}
-            />
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onToggle={() => toggleTodo(todo.id)}
+            onDelete={() => deleteTodo(todo.id)}
+          />
         ))}
       </ul>
-       
-      {deleteModal && (
-              <div className="fixed inset-0 w-full bg-opacity-100 border-10 flex flex-col justify-center items-center">
-                <div className="shadow-md shadow-[#5b21b6]/20 bg-white border-solid border-2 border-[#8b5cf6] w-96 h-40 flex flex-col justify-center items-center rounded-3xl ">
-                  <h2 className="mt-2 mb-6 text-xl font-semibold">정말 삭제하시겠습니까?</h2>
-                    <div className="flex">
-                      <button className="mr-5 w-32 justify-self-end p-2 mt-1 mb-3 bg-[#8b5cf6] text-white border border-[#8b5cf6] rounded-2xl hover:bg-white hover:text-[#8b5cf6]"
-                        onClick={() => {
-                          deleteTodo(deleteID);
-                          setDeleteModal(false);
-                        }}
-                      >삭제</button>
-                      <button className="w-32 justify-self-end p-1 mt-1 mb-3 bg-[#8b5cf6] text-white border border-[#8b5cf6] rounded-2xl hover:bg-white hover:text-[#8b5cf6]"
-                        onClick={() => setDeleteModal(false)}
-                      >취소</button>
-                    </div>
-                </div>
-              </div>
-            )}
     </div>
   );
 };
-
-
 
 export default TodoList;
